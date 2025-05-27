fe7a4b852c9d0b8ca12a77095b66ba059bc227b8
Peter Szasz <peter.szasz333@gmail.com>
2025-05-27 12:21:12 +0200

    triangle calculations

---
+++ math_tasks/triangeel.py
@@ -0,0 +1,18 @@
+import math as m
+
+def calculate_triangle_angles(a, b, c):
+    if a or b or c > 0:
+        alfa = (c**2+b**2-a**2)/(2*b*c)
+        beta = (a**2+c**2-b**2)/(2*a*b)
+        gama = (a**2+b**2-c**2)/(2*a*b)
+        alfa = round(m.degrees(m.acos(alfa)), 2)
+        beta = round(m.degrees(m.acos(beta)), 2)
+        gama = round(m.degrees(m.acos(gama)), 2)
+        return alfa, beta, gama
+    else:
+        return 0, 0, 0, 0
+
+
+
+
+
---
+++ test/math_tasks/test_triangeel.py
@@ -0,0 +1,26 @@
+
+from unittest import TestCase
+
+
+from math_tasks.triangeel import calculate_triangle_angles
+
+class TestTriangle(TestCase):
+
+
+    def test_calculate_triangle_angles(self):
+        
+        angle = calculate_triangle_angles(5, 6, 7)
+        talfa = 44.42
+        tbeta = 50.7
+        tgama = 78.46
+        self.assertEqual(talfa, angle[0])
+        self.assertEqual(tbeta, angle[1])
+        self.assertEqual(tgama, angle[2])
+
+    def test_calculate_triangle_angles_negative(self):
+        result = calculate_triangle_angles(-1, 0, 5)
+        self.assertEqual(0, angle[0])
+        self.assertEqual(0, angle[1])
+        self.assertEqual(0, angle[2])
+
+

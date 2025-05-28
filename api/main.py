0755aedf2c1bbdb2ad3cbc6bb5786aec417a5695
peter <peter.szasz333@gmail.com>
2025-05-28 10:15:06 +0200

    add sqrt number

--- api/main.py
+++ api/main.py
@@ -3,6 +3,7 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from math_tasks.sqrt import sqrt_number
 app = FastAPI()
 
 
@@ -25,3 +26,8 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/sqrt/{number}")
+def sqrt(number: float):
+    sqrted_number = sqrt_number(number)
+    return {"number": number, "sqrted_number": sqrted_number}

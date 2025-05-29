02fc1b14c49f750183b0ddb7194d4d928e6d02d4
peter <peter.szasz333@gmail.com>
2025-05-29 09:38:27 +0200

    update

--- math_tasks/quadratic_functions.py
+++ math_tasks/quadratic_functions.py
@@ -1,11 +1,14 @@
 def root_of_quadratic_fun(a, b, c):
     discriminant = b**2 - 4*a*c
-    if discriminant < 0:
-        return None, None
-    elif discriminant == 0:
-        x = -b / (2*a)
-        return x, None
+    if a != 0:
+        if discriminant < 0:
+            return None, None
+        elif discriminant == 0:
+            x = -b / (2*a)
+            return x, None
+        else:
+            x1 = (-b + discriminant) / (2*a)
+            x2 = (-b - discriminant) / (2*a)
+            return x1, x2
     else:
-        x1 = (-b + discriminant) / (2*a)
-        x2 = (-b - discriminant) / (2*a)
-        return x1, x2
+        return None, None

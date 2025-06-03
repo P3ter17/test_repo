993b98cb63d9859f2779d416007f233a06733aaa
peter <peter.szasz333@gmail.com>
2025-06-03 10:44:22 +0200

    fix mistake

--- math_tasks/triangeel.py
+++ math_tasks/triangeel.py
@@ -6,7 +6,7 @@ def calculate_triangle_angles(a, b, c):
         small_mid = (a + b + c) - biggest_side
         if small_mid > biggest_side:
             alfa = (c**2+b**2-a**2)/(2*b*c)
-            beta = (a**2+c**2-b**2)/(2*a*b)
+            beta = (a**2+c**2-b**2)/(2*a*c)
             gama = (a**2+b**2-c**2)/(2*a*b)
             alfa = round(m.degrees(m.acos(alfa)), 2)
             beta = round(m.degrees(m.acos(beta)), 2)
@@ -16,7 +16,6 @@ def calculate_triangle_angles(a, b, c):
             return None, None, None
     else:
         return None, None, None
-
 
 
 
